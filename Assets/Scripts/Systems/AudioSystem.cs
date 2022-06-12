f347dee6dca10e371536fc7f5a297a49c82f8cb4
Игорь Чудакин <Chudakin98@gmail.com>
2022-06-12 20:54:22 +0700

    Добавлено изменение звука при погружении в воду и выходе на поверхность (можно добавлять ещё моды)

---
+++ Assets/Scripts/Systems/AudioSystem.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSystem : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioMode> audioModes;
+
+    private AudioSource[] sources;
+
+    private void Start()
+    {
+        sources = FindObjectsOfType<AudioSource>();
+    }
+
+    public void SetWaterAudioMode()
+    {
+        AudioMode mode = FindAudioModeOfType(AudioModeType.Water);
+        SetModeForAllSources(mode);
+    }
+    public void SetStandardAudioMode()
+    {
+        AudioMode mode = FindAudioModeOfType(AudioModeType.Standard);
+        SetModeForAllSources(mode);
+    }
+
+    private void SetModeForAllSources(AudioMode mode)
+    {
+        for (int i = 0; i < sources.Length; i++)
+        {
+            sources[i].outputAudioMixerGroup = mode.mixer;
+        }
+    }
+    private AudioMode FindAudioModeOfType(AudioModeType type)
+    {
+        foreach (var item in audioModes)
+        {
+            if(item.modeType == type)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+}
+
+[System.Serializable]
+public class AudioMode
+{
+    public AudioModeType modeType;
+    public AudioMixerGroup mixer;
+}
+
+public enum AudioModeType
+{
+    Standard,
+    Water
+}
