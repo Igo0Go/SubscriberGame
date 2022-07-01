6df19e075aec53d4f9a0c38ed740fb695993880a
Игорь Чудакин <Chudakin98@gmail.com>
2022-07-01 17:52:18 +0700

    Добавлен бот и базовые анимации разговора

---
+++ Assets/Scripts/Player/BotController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum AnimType
+{
+    SimpleTalk = 1,
+    JoyTalk = 2,
+    SadTalk = 3,
+    JustThink = 4,
+    FindDecision = 5,
+}
